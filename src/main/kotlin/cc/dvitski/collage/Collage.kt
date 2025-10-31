26ffd3fd46de9ed5c5238a4d93d5bb511482315c
Daniel Orr <orrdaniel73@gmail.com>
2025-10-31 15:40:50 +0000

    feat: filename

--- src/main/kotlin/cc/dvitski/collage/Collage.kt
+++ src/main/kotlin/cc/dvitski/collage/Collage.kt
@@ -4,10 +4,15 @@ import cc.dvitski.collage.packet.ServerboundScreenshotPayload
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
+import net.minecraft.Util
 import net.minecraft.network.chat.Component
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.level.storage.LevelResource
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.ByteArrayInputStream
+import javax.imageio.ImageIO
 
 object Collage : ModInitializer {
     const val MOD_ID = "collage"
@@ -21,16 +26,30 @@ object Collage : ModInitializer {
         PayloadTypeRegistry.playC2S().register(ServerboundScreenshotPayload.TYPE, ServerboundScreenshotPayload.STREAM_CODEC)
 
         ServerPlayNetworking.registerGlobalReceiver(ServerboundScreenshotPayload.TYPE) { payload, context ->
-            val player = context.player()
-            player.sendSystemMessage(Component.literal("Received screenshot (${payload.bytes.size}B)"), true)
-
-            val server = context.server()
-            val path = server.getWorldPath(LevelResource("$MOD_ID/screenshots"))
-            val file = path.toFile()
-            file.mkdirs()
-            file.resolve("screenshot.png").writeBytes(payload.bytes)
-
-            player.sendSystemMessage(Component.literal("Saved screenshot (${payload.bytes.size}B)"), true)
+            handleScreenshot(context.player(), context.server(), payload.bytes)
         }
+    }
+
+    private fun handleScreenshot(player: ServerPlayer, server: MinecraftServer, bytes: ByteArray) {
+        if (!validatePng(bytes)) {
+            return
+        }
+
+        player.sendSystemMessage(Component.literal("Received screenshot (${bytes.size}B)"), true)
+
+        val filename = Util.getFilenameFormattedDateTime()
+
+        val path = server.getWorldPath(LevelResource("$MOD_ID/screenshots"))
+        val file = path.toFile()
+        file.mkdirs()
+        file.resolve("$filename.png").writeBytes(bytes)
+
+        player.sendSystemMessage(Component.literal("Server saved screenshot ($filename)"), true)
+    }
+
+    private fun validatePng(bytes: ByteArray): Boolean {
+        return runCatching {
+            ImageIO.read(ByteArrayInputStream(bytes))
+        }.getOrNull() != null // not isSuccess, result of ImageIO.read can be null
     }
 }
